7deaec3c601ee0f91f4019b707eca8cd3a8481aa
Rxiaou <230852910@qq.com>
2022-10-19 11:33:03 +0800

    cesium暗色滤镜

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # CesiumDarkMap
-cesium暗色地图代码(不需要修改源码)
+cesium暗色地图滤镜代码(不需要修改源码)
+
+个人用的高德
+
+描述:
+
+```
+google找了半天的方法,发出来让大家少走弯路,用于实现暗色地图,国内的地图用不了暗色,超图地图不太全,所以就有了这种需求
+```
+
+使用方法
+引入:
+
+```
+ import modifyMap from './filterColor'
+ //根据实际情况修改,你想放哪里就放哪里,不过记得填正确的路径
+```
+
+调用:
+
+```
+modifyMap(viewer, {
+    //反色?
+    invertColor: true,
+    //滤色值
+    filterRGB: [60, 145, 172],
+});
+//viewer是什么不用我说了吧
+```
+
+(注:filterColor.js中我是`import * as Cesium from 'cesium'` 这样引入cesium的,请根据实际情况修改)
+
+效果如图:
+
+![demo](.\demo.png)
---
+++ filterColor.js
@@ -0,0 +1,42 @@
+import * as Cesium from 'cesium'//根据自己的实际路径修改
+
+export default function modifyMap(viewer, options) {
+    const baseLayer = viewer.imageryLayers.get(0)
+    //以下几个参数根据实际情况修改,目前我是参照火星科技的参数,个人感觉效果还不错
+    baseLayer.brightness = options.brightness || 0.6
+    baseLayer.contrast = options.contrast || 1.8
+    baseLayer.gamma = options.gamma || 0.3
+    baseLayer.hue = options.hue || 1
+    baseLayer.saturation = options.saturation || 0
+    const baseFragShader = (viewer.scene.globe)._surfaceShaderSet
+        .baseFragmentShaderSource.sources
+    for (let i = 0; i < baseFragShader.length; i++) {
+        const strS = 'color = czm_saturation(color, textureSaturation);\n#endif\n'
+        let strT = 'color = czm_saturation(color, textureSaturation);\n#endif\n'
+        if (options.invertColor) {
+            strT += `
+      color.r = 1.0 - color.r;
+      color.g = 1.0 - color.g;
+      color.b = 1.0 - color.b;
+      `
+        }
+        if (options.filterRGB.length > 0) {
+            strT += `
+      color.r = color.r * ${options.filterRGB[0]}.0/255.0;
+      color.g = color.g * ${options.filterRGB[1]}.0/255.0;
+      color.b = color.b * ${options.filterRGB[2]}.0/255.0;
+      `
+        }
+        baseFragShader[i] = baseFragShader[i].replace(strS, strT)
+    }
+}
+
+//调用
+// import modifyMap from './filterColor'
+modifyMap(viewer, {
+    //反色?
+    invertColor: true,
+    //滤镜值
+    filterRGB: [60, 145, 172],
+});
+viewer是什么不用我说了吧
